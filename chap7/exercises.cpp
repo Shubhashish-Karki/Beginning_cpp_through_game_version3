3de0f4e375ba5e512e82778fcc3607d027a6ca3b
Shubhashish Karki <shubhashishkarki@gmail.com>
2024-08-31 11:29:09 +0545

    exercise complete

---
+++ chap7/exercises.cpp
@@ -0,0 +1,19 @@
+///// ex 1///
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    // string d ="hello world";
+    // string *str =&d;
+
+    // cout <<str->size() << endl;
+    int a = 10;
+    int& b = a;
+    int* c = &b;
+    cout << &a << endl;
+    cout << &b << endl;
+    cout << &(*c) << endl;
+}
